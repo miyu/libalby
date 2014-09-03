9df2264532b88e79408b27900890fbd47b2c8d84
Michael Yu <ItzWarty@gmail.com>
2014-09-02 20:09:13 -0700

    Mouse Subsystem exposes left/middle/right/xbutton1,2/wheeldelta.

--- libhelios/MouseSubsystem.cs
+++ libhelios/MouseSubsystem.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using SharpDX.Toolkit;
 using SharpDX.Toolkit.Input;
 using SharpDX.Windows;
@@ -36,8 +35,13 @@ namespace Shade.Helios
          mouseX = (int)(mouseState.X * graphicsDeviceManager.PreferredBackBufferWidth);
          mouseY = (int)(mouseState.Y * graphicsDeviceManager.PreferredBackBufferHeight);
       }
-
       public int X { get { return mouseX; } }
       public int Y { get { return mouseY; } }
+      public ButtonState Left { get { return mouseState.LeftButton; } }
+      public ButtonState Middle { get { return mouseState.MiddleButton; } }
+      public ButtonState Right { get { return mouseState.RightButton; } }
+      public ButtonState XButton1 { get { return mouseState.XButton1; } }
+      public ButtonState XButton2 { get { return mouseState.XButton2; } }
+      public int WheelDelta { get { return mouseState.WheelDelta; } }
    }
 }
